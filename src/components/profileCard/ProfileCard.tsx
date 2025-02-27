bf0ee9dbba214d75c0bf2c93b29d7bea2f9a30d3
Admin <1119321.ab@mail.ru>
2025-02-27 16:31:09 +0100

    h2

--- src/components/profileCard/ProfileCard.tsx
+++ src/components/profileCard/ProfileCard.tsx
@@ -1,37 +1,26 @@
-// import './ProfileCard.css'
-// export default function ProfileCard({ avatar, firstname, lastname, profession, hobby }) {
-//     return (
-//         <div className="profile-card">
-//                 <img src={avatar} alt={`${firstname} ${lastname}`} style={{ width: '100px', height: '100px' }} />
-//             <p>Name: {firstname} {lastname}</p>
-//             <p>Profession: {profession}</p>
-//             <p>Hobby: {hobby}</p>
-//         </div>
-//     );
-// }
-
-import './ProfileCard.css'
+import styles from "./profileCard.module.css";
 
 interface IProfileCardProps {
-    avatar: string;
-    firstname: string;
-    lastname: string;
-    profession: string;
-    hobby: string;
+  avatar: string;
+  firstname: string;
+  lastname: string;
+  profession: string;
+  hobby: string;
 }
 
-
-export default function ProfileCard({ avatar, firstname, lastname, profession, hobby }: IProfileCardProps) {
-    return (
-        <>
-    
-                <div className="profile-card">
-                    <img src={avatar} alt={`${firstname} ${lastname}`} style={{ width: '100px', height: '100px' }} />
-                    <p>Name: {firstname} {lastname}</p>
-                    <p>Profession: {profession}</p>
-                    <p>Hobby: {hobby}</p>
-                </div>
-            
-        </>
-    );
+export default function ProfileCard({
+  avatar,
+  firstname,
+  lastname,
+  profession,
+  hobby,
+}: IProfileCardProps) {
+  return (
+    <div className={styles["profile-card"]}>
+      <img src={avatar} alt={`${firstname} ${lastname}`} />
+      <p>Name: {firstname} {lastname}</p>
+      <p>Profession: {profession}</p>
+      <p>Hobby: {hobby}</p>
+    </div>
+  );
 }
